5b57f82eec4c2f52e36e35fc127f76d640b1a4e3
Ryan Bitner <79929655+ryanbitner01@users.noreply.github.com>
2026-04-25 02:27:43 -0600

    feat:change auto switching theme

--- merch-website/src/main.tsx
+++ merch-website/src/main.tsx
@@ -4,6 +4,21 @@ import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router";
 import MerchList from "./components/merch/merchList/merchList.tsx";
 
+const mediaQuery = window.matchMedia("(prefers-color-scheme: light)");
+
+const applyTheme = (e: MediaQueryList | MediaQueryListEvent) => {
+	document.documentElement.setAttribute(
+		"data-bs-theme",
+		e.matches ? "light" : "dark",
+	);
+};
+
+// set initial theme
+applyTheme(mediaQuery);
+
+// listen for changes
+mediaQuery.addEventListener("change", applyTheme);
+
 createRoot(document.getElementById("root")!).render(
 	<BrowserRouter>
 		<Routes>
